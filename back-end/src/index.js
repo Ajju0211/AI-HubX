e9e599b841342d23b2753f5bdffafb0a0ad37c82
Ajju0211 <ajaywork0211@gmail.com>
2025-02-05 12:11:53 +0530

    Error Solved

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -49,11 +49,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+  app.use(express.static(path.join(__dirname, "front-end", "dist")));  // For Vite
   
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../front-end", "dist", "index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "front-end", "dist", "index.html")); // For Vite
+  });
   }
 
 const PORT = process.env.PORT || 3000;
