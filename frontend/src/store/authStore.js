dc7a87a05f2da88326c5390c002d477492a08d03
Ajay_singh <ajaywork0211@gmail.com>
2025-03-08 22:58:43 +0530

    Update authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,9 +2,9 @@ import { create } from "zustand";
 import axios from "axios";
 import { socket } from "../socket/socket";
 
-const API_URL = "https://ai-hubx-env.up.railway.app/api/auth";
+const API_URL = "https://ai-hubx.up.railway.app/api/auth";
 // "https://full-stack-gemini-clone-1.onrender.com/api/auth";
-const API_URL_CHAT = "https://ai-hubx-env.up.railway.app/api/chat";
+const API_URL_CHAT = "https://ai-hubx.up.railway.app/api/chat";
 // "https://full-stack-gemini-clone-1.onrender.com/api/chat";
 // Ensure credentials are always sent with requests
 axios.defaults.withCredentials = true;
