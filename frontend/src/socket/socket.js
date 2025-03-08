1ad7b703eb44b864763dd1672904ba05393ca7b8
Ajay_singh <ajaywork0211@gmail.com>
2025-03-08 23:26:09 +0530

    Update socket.js

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 
-const  SOCKET_URL = 'http://localhost:3000';
+const  SOCKET_URL = 'https://ai-hubx-env.up.railway.app';
 export const socket = io(SOCKET_URL, {
     withCredentials: true, // Sends cookies (for authentication)
     autoConnect: false, // Prevents auto-connection; connects only when needed
