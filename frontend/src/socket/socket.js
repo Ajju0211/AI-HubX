09fd1c99e44249a415d96aa8494b413e209d37aa
Ajay0211 <ajaysdoriyal@gmail.com>
2025-03-10 18:51:54 +0530

    Feature break

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,13 +1,9 @@
 import { io } from 'socket.io-client';
 
 
-<<<<<<< HEAD
 // const  SOCKET_URL =  'https://ai-hubx-env.up.railway.app';
 const  SOCKET_URL = import.meta.env.ALLOWED_ORIGIN || 'http://localhost:3000';
 
-=======
-const  SOCKET_URL = 'https://ai-hubx.up.railway.app';
->>>>>>> 7c94fbe349240432ebf0a2dad4d94583d025b1e5
 export const socket = io(SOCKET_URL, {
     withCredentials: true, // Sends cookies (for authentication)
     autoConnect: false, // Prevents auto-connection; connects only when needed
