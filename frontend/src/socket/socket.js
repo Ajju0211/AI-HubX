a7ddcff3c50f3e4aea348753d49fef2d0bdb7562
Ajay_singh <ajaywork0211@gmail.com>
2025-06-07 13:21:14 +0530

    Update socket.js

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 
 
-const  SOCKET_URL = import.meta.env.ENV ==='production' ?  import.meta.env.API_URL : 'http://localhost:3000';
+const  SOCKET_URL = import.meta.env.MODE ==='production' ?  import.meta.env.API_URL : 'http://localhost:3000';
 
 export const socket = io(SOCKET_URL, {
     withCredentials: true, // Sends cookies (for authentication)
